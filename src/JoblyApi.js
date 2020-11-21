e534cdb6ef314e9eb3eeb0cbbf4b46f013c5a242
Emma Huynh <emmahuynh@Emmas-MBP.fios-router.home>
2020-11-20 18:37:42 -0800

    removing logs

--- src/JoblyApi.js
+++ src/JoblyApi.js
@@ -2,8 +2,6 @@ import axios from "axios";
 import { TOKEN_LOCALSTORAGE } from "./App.js"
 
 const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
-// const BASE_URL = "https://tnhuynh-react-jobly-backend.herokuapp.com";
-console.log("JoblyApi - BASE_URL =", BASE_URL);
 
 class JoblyApi {
     static async request(endpoint, paramsOrData = {}, verb = "get") {
@@ -41,19 +39,16 @@ class JoblyApi {
 
     static async getCompanies(search) {
         let res = await this.request(`companies`, { search });
-        console.log("API CALL - GET COMPANIES RESULT", res);
         return res.companies;
     }
 
     static async getCompany(handle) {
         let res = await this.request(`companies/${handle}`);
-        console.log("API CALL - GET SINGLE COMPANY RESULT", res);
         return res.company;
     }
 
     static async getJobs(search) {
         let res = await this.request(`jobs`, { search });
-        console.log("API CALL - GET ALL JOBS RESULT", res);
         return res.jobs;
     }
 
@@ -64,25 +59,21 @@ class JoblyApi {
 
     static async login(data) {
         let res = await this.request(`login`, data, "post");
-        console.log("API CALL - LOGIN", res);
         return res.token;
     }
 
     static async register(data) {
         let res = await this.request(`users`, data, "post");
-        console.log("API CALL - REGISTER", res);
         return res.token;
     }
 
     static async getCurrentUser(username) {
         let res = await this.request(`users/${username}`);
-        console.log("API CALL - GET CURRENT USER", res);
         return res.user;
     }
 
     static async saveProfile(username, data) {
         let res = await this.request(`users/${username}`, data, "patch");
-        console.log("API CALL - SAVE PROFILE", res);
         return res.user;
     }
 }
